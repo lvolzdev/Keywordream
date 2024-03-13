13db22349eb4cec96ab7b1ceb4a68d1a4adabdae
김영석 <kimyoungseok15@gmail.com>
2024-03-13 13:45:06 +0900

    feat: footer 화면 맨 아래로 고정

--- src/components/NavLayout.jsx
+++ src/components/NavLayout.jsx
@@ -7,27 +7,22 @@ import { Outlet } from "react-router-dom";
 export default function NavLayout() {
   return (
     <>
-      <div style={{ display: "flex", minHeight: "100vh" }}>
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
+      <div
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
+        <div style={{ flex: 1 }}>
           <Header />
           <Container
             style={{
               backgroundColor: "#ffffff",
+              flexGrow: 1,
             }}
           >
             <Outlet />
           </Container>
-          <Footer />
         </div>
+        <Footer />
       </div>
     </>
   );
 }
-
-<div>NavLayout</div>;
