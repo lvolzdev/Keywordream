9c518fb619155728d196adb5dfbd056848afa181
김영석 <kimyoungseok15@gmail.com>
2024-03-13 13:11:12 +0900

    feat: 랜딩페이지 임시 완성

--- src/routes/Landing/Landing.jsx
+++ src/routes/Landing/Landing.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import RunningBear from "../../assets/image/RunningBear.gif";
+import Logo from "../../assets/image/logo.png";
+import styles from "./Landing.module.css";
+import Button from "@mui/material/Button";
 
 export default function Landing() {
   return (
-    <div>
-      <h1>LandingPage</h1>
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src={Logo} alt="" className={styles.logo} />
+      </div>
+      <div className={styles.bearContainer}>
+        <img src={RunningBear} alt="" className={styles.bear} />
+      </div>
+      <div className={styles.buttonContainer}>
+        <Button variant="contained" href="/main" className={styles.button}>
+          시작하기
+        </Button>
+      </div>
     </div>
   );
 }
