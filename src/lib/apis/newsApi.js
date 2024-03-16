063b4413452151b70afa0554ec280136def38e29
kihyun.kwon <kkh3272@naver.com>
2024-03-16 22:26:06 +0900

    feat: add news page with dummy data

---
+++ src/lib/apis/newsApi.js
@@ -0,0 +1,50 @@
+// import instance from "./baseApi";
+
+export const getNewsList = (stockCode) => {
+    //TODO 추후에 실제 api와 연결
+    const data = [
+        {
+            id:"1",
+            title: "'번호이동 지원금' 오늘부터 최대 13만원까지",
+            press: "노컷뉴스",
+            date: "10분 전",
+            imgUrl: "https://imgnews.pstatic.net/image/origin/079/2024/03/16/3874535.jpg?type=nf220_150",
+        },
+        {
+            id:"2",
+            title: "'번호이동 지원금' 오늘부터 최대 13만원까지",
+            press: "노컷뉴스",
+            date: "10분 전",
+            imgUrl: "https://imgnews.pstatic.net/image/origin/079/2024/03/16/3874535.jpg?type=nf220_150",
+        },
+        {
+            id:"3",
+            title: "'번호이동 지원금' 오늘부터 최대 13만원까지",
+            press: "노컷뉴스",
+            date: "10분 전", 
+            imgUrl: "https://imgnews.pstatic.net/image/origin/079/2024/03/16/3874535.jpg?type=nf220_150",
+        },
+        {
+            id:"4",
+            title: "'번호이동 지원금' 오늘부터 최대 13만원까지",
+            press: "노컷뉴스",
+            date: "10분 전", 
+            imgUrl: "https://imgnews.pstatic.net/image/origin/079/2024/03/16/3874535.jpg?type=nf220_150",
+        },
+        {
+            id:"5",
+            title: "'번호이동 지원금' 오늘부터 최대 13만원까지",
+            press: "노컷뉴스",
+            date: "10분 전",
+            imgUrl: "https://imgnews.pstatic.net/image/origin/079/2024/03/16/3874535.jpg?type=nf220_150",
+        },
+        {
+            id:"6",
+            title: "'번호이동 지원금' 오늘부터 최대 13만원까지",
+            press: "노컷뉴스",
+            date: "10분 전",
+            imgUrl: "https://imgnews.pstatic.net/image/origin/079/2024/03/16/3874535.jpg?type=nf220_150",
+        }
+    ]
+    return data;
+};
--- src/routes/News/News.jsx
+++ src/routes/News/News.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import Keyword from "./Keyword";
+import { getNewsList } from "../../lib/apis/newsApi";
+import NewsList from "./NewsList";
 
 export default function News() {
-  return <div>News</div>;
+  const stockCode = useParams().stockCode
+  const [keywords, setKeywords] = useState(["키워드1", "키워드2", "키워드3"])
+  const [newsList, setNewsList] = useState([])
+
+  useEffect(()=>{
+    //TODO 주식코드에 따라서 키워드 & 뉴스 리스트 받아오기
+    setNewsList(getNewsList(stockCode))
+  },[stockCode])
+
+  return(
+    <div>
+      <Keyword keywords={keywords}/>
+      <NewsList newsList={newsList} stockCode={stockCode}/>
+    </div>
+  );
 }
