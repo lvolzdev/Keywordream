00f18c7cb8397137d2f52264cdcdefd408f6cdb7
kihyun.kwon <kkh3272@naver.com>
2024-03-20 18:06:40 +0900

    feat: add news detail api to news detail page

--- src/routes/News/NewsDetail.jsx
+++ src/routes/News/NewsDetail.jsx
@@ -2,6 +2,7 @@ import "./NewsDetail.css";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getNewsDetail } from "../../lib/apis/newsApi";
+import { decodeHTMLEntities } from "../../components/decode/htmlToValue";
 
 export default function NewsDetail() {
   const newsId = useParams().newsId
@@ -9,13 +10,28 @@ export default function NewsDetail() {
     title : "",
     newsDate : "",
     press : "",
-    isGood : "", //TODO 변수명 바뀔 수 있음
+    isGood : null, //TODO 변수명 바뀔 수 있음
     content : ""
   })
 
   useEffect(() => {
-    setData(getNewsDetail(newsId))
+    getNewsDetailData();
   }, [newsId])
+
+  useEffect(() => {
+    if(data.isGood === null){
+      //TODO CHAT_GPT 실행????
+    }
+  }, [data.isGood])
+
+  const getNewsDetailData = async () => {
+    try{
+      const newsTagTop3Data = await getNewsDetail(newsId);
+      setData(newsTagTop3Data);
+    } catch(error){
+      console.log("뉴스 디테일 페이지 오류: ",error)
+    }
+  }
 
   const eventStyle = {
     fontWeight:700,
@@ -24,19 +40,27 @@ export default function NewsDetail() {
 
   return (
     <div className="newsDetailLayout">
-      <div className="newsDetailTitle">{data.title}</div>
+      <div className="newsDetailTitle">{decodeHTMLEntities(data.title)}</div>
       <div className="newsDetailPressDate">
         <div className="newsDetailPress">{data.press}</div>
         <div className="newsDetailDate">{data.newsDate}</div>
       </div>
       <div className="newsDetailJudgment">
-        <img
-          className="newsDetailChatGPTLogo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/2048px-ChatGPT_logo.svg.png"
-        />
-        <div>ChatGPT가 이 뉴스를 <span style={eventStyle}>{data.isGood}</span>로 판단하였습니다.</div>
+        {
+          data.isGood !== null ? (
+            <div>
+              <img className="newsDetailChatGPTLogo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/2048px-ChatGPT_logo.svg.png"
+              />
+              <div>ChatGPT가 이 뉴스를 <span style={eventStyle}>{data.isGood}</span>로 판단하였습니다.</div>
+            </div>
+          ) : (
+            <></>
+          )
+        }
       </div>
       <div dangerouslySetInnerHTML={{__html: data.content}}></div>
+      <div className="newDetailFoot"></div>
     </div>
   )
 }
