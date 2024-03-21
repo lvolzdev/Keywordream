640711e3cd8c7ebb6c39b562d59a0a9d1fd2f005
김영석 <kimyoungseok15@gmail.com>
2024-03-21 17:59:50 +0900

    feat: 디테일 페이지 상단 이미지와 기업명 구현

--- src/components/DetailLayout.jsx
+++ src/components/DetailLayout.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { Outlet, useParams } from "react-router-dom";
-import stockimage from "./005930.png";
 import "./DetailLayout.css"; // CSS 파일 import
 import styles from "../routes/Main/PopularStock.module.css";
 import Tabs from "@mui/material/Tabs/";
 import Tab from "@mui/material/Tab/";
 import { Link } from "react-router-dom";
+import { fetchStockInfo } from "../lib/apis/stockInfo";
 
 export default function DetailLayout() {
   const [tabIndex, setTabIndex] = useState(0);
-  const stockCode = useParams().stockCode
+  const [stockName, setStockName] = useState("");
+  const stockCode = useParams().stockCode;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchStockInfo(stockCode); // stockCode를 인자로 전달하여 호출
+        setStockName(data);
+      } catch (error) {
+        console.error("Error fetching stockInfo:", error);
+      }
+    };
 
+    fetchData();
+  }, [stockCode]); // stockCode가 변경될 때마다 호출
   const handleChange = (event, newValue) => {
     setTabIndex(newValue);
   };
 
   return (
-    <div className="detail-container"> {/* 클래스명을 추가하여 CSS 적용 */}
+    <div className="detail-container">
+      {" "}
+      {/* 클래스명을 추가하여 CSS 적용 */}
       <div className="detail-icon">
-        <img src={stockimage} alt="stock_image" style={{ width: "3em", height: "3em", borderRadius: "30px" }} />
-        <p style={{ marginLeft: "1rem", fontWeight: "bold" }}>삼성전자</p>
+        <img
+          src={`https://file.alphasquare.co.kr/media/images/stock_logo/kr/${stockCode}.png`}
+          alt=""
+          className={styles.stockImg}
+          onError={(e) => {
+            e.target.src =
+              "https://file.alphasquare.co.kr/media/images/stock_logo/ETF_230706.png";
+          }}
+          style={{ width: "3em", height: "3em", borderRadius: "30px" }}
+        />
+        <p style={{ marginLeft: "1rem", fontWeight: "bold" }}>{stockName}</p>
         <div className="detail-num">
           <p style={{ fontWeight: "bold" }}>74,300</p>
           <p style={{ fontSize: "0.7rem" }}>KRW</p>
@@ -73,7 +96,6 @@ export default function DetailLayout() {
           }}
         />
       </Tabs>
-
       <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
         <Container>
           <Outlet />
@@ -82,4 +104,3 @@ export default function DetailLayout() {
     </div>
   );
 }
-
