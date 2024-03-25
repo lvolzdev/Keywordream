4ab368194486cee69662701fcc2afbf678246cb7
김영석 <kimyoungseok15@gmail.com>
2024-03-25 09:44:59 +0900

    feat: 메인페이지 인기종목 가격 및 등락율 초기값 api로 가져오기

--- src/lib/apis/stockPrice.js
+++ src/lib/apis/stockPrice.js
@@ -3,7 +3,7 @@ import instance from "./baseApi";
 // 종목 코드로 현재가 가져오기
 export const fetchStockPrice = async (stockCode) => {
   try {
-    const response = await instance.get(`/main/${stockCode}`);
+    const response = await instance.get(`/main/price/${stockCode}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching stockInfo:', error);
