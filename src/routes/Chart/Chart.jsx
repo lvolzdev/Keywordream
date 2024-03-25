af5f1e5a15b3c629400ab336f772f454f8c145f3
Sieun Kim <se0983@kookmin.ac.kr>
2024-03-25 16:39:46 +0900

    feat: candle chart 그리기

--- src/routes/Chart/Chart.jsx
+++ src/routes/Chart/Chart.jsx
@@ -1,151 +1,255 @@
-import React from "react";
-import React, { useEffect, useState, useMemo } from "react";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
-import ApexChart from "react-apexcharts";
-import Button from "@mui/material/Button";
+import React, { useEffect, useState, useRef } from "react";
 import { getChart } from "../../lib/apis/chartApi";
+import { useParams } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { format } from "d3-format";
+import { timeFormat } from "d3-time-format";
+import {
+  elderRay,
+  ema,
+  discontinuousTimeScaleProviderBuilder,
+  Chart,
+  ChartCanvas,
+  CurrentCoordinate,
+  BarSeries,
+  CandlestickSeries,
+  ElderRaySeries,
+  LineSeries,
+  MovingAverageTooltip,
+  OHLCTooltip,
+  SingleValueTooltip,
+  lastVisibleItemBasedZoomAnchor,
+  XAxis,
+  YAxis,
+  CrossHairCursor,
+  EdgeIndicator,
+  MouseCoordinateX,
+  MouseCoordinateY,
+  ZoomButtons,
+  withDeviceRatio,
+  withSize,
+} from "react-financial-charts";
 
-export default function Chart() {
-  const colors = useMemo(() => ["#FE2F4D", "#1545FF"], []);
+export default function StockChart() {
   const stockCode = useParams().stockCode;
   const [chartData, setChartData] = useState([]);
-  const [chartColor, setChartColor] = useState(colors[0]);
-  const [isCandleChart, setIsCandleChart] = useState(false); // 라인 차트와 캔들 차트 토글
+  const chartContainerRef = useRef(null);
 
   useEffect(() => {
-    const fetchChart = async () => {
+    const fetchChartData = async () => {
       try {
         const response = await getChart(stockCode);
-        // console.log(response.data.t8412OutBlock1);
-
-        if (response) {
-          // 시세 데이터 처리
-          const chartSeriesData = response.data.t8412OutBlock1.map((res) => ({
-            x: `${res.date.slice(0, 4)}-${res.date.slice(
-              4,
-              6
-            )}-${res.date.slice(6, 8)}T${res.time.slice(0, 2)}:${res.time.slice(
-              2,
-              4
-            )}:00.000Z`,
-            y: Number(res.close),
-          }));
-          setChartData(chartSeriesData);
-
-          // 시세 데이터 추세에 따라 그래프 색상 변경
-          var maxVal = chartSeriesData[0].y;
-          var minVal = chartSeriesData[0].y;
-          var maxDate = new Date(chartSeriesData[0].x);
-          var minDate = new Date(chartSeriesData[0].x);
-
-          chartSeriesData.forEach((data) => {
-            if (data.y > maxVal) {
-              maxVal = data.y;
-              maxDate = new Date(data.x);
-            }
-            if (data.y < minVal) {
-              minVal = data.y;
-              minDate = new Date(data.x);
-            }
-          });
-
-          setChartColor(maxDate > minDate ? colors[0] : colors[1]);
-
-          // const trend =
-          //   chartSeriesData[chartSeriesData.length - 1].y -
-          //   chartSeriesData[0].y;
-          // setChartColor(trend >= 0 ? colors[0] : colors[1]);
-        }
+        const data = response.data.t8412OutBlock1.map((res) => ({
+          date: `${res.date.slice(0, 4)}-${res.date.slice(
+            4,
+            6
+          )}-${res.date.slice(6, 8)}T${res.time.slice(0, 2)}:${res.time.slice(
+            2,
+            4
+          )}:00.000Z`,
+          open: Number(res.open),
+          close: Number(res.close),
+          high: Number(res.high),
+          low: Number(res.low),
+        }));
+        setChartData(data);
       } catch (error) {
-        console.error("Error fetching chart data:", error);
+        console.log(error);
       }
     };
-    fetchChart();
-  }, [stockCode, colors]);
 
-  const seriesData = chartData.map((data) => {
-    return {
-      x: data.x, // 날짜 문자열
-      y: data.y, // 가격
-    };
-  });
-  // console.log(seriesData);
+    fetchChartData();
+  }, [stockCode]);
+
+  // Candle Chart
+  const ScaleProvider =
+    discontinuousTimeScaleProviderBuilder().inputDateAccessor(
+      (d) => new Date(d.date)
+    );
+  const height = 500;
+  const width = 350;
+  const margin = { left: 0, right: 48, top: 15, bottom: 24 };
+
+  const ema12 = ema()
+    .id(1)
+    .options({ windowSize: 12 })
+    .merge((d, c) => {
+      d.ema12 = c;
+    })
+    .accessor((d) => d.ema12);
+
+  const ema26 = ema()
+    .id(2)
+    .options({ windowSize: 26 })
+    .merge((d, c) => {
+      d.ema26 = c;
+    })
+    .accessor((d) => d.ema26);
+
+  const elder = elderRay();
+
+  const calculatedData = elder(ema26(ema12(chartData)));
+  const { data, xScale, xAccessor, displayXAccessor } =
+    ScaleProvider(chartData);
+  const pricesDisplayFormat = format(".2f");
+  const max = xAccessor(data[data.length - 1]);
+  const min = xAccessor(data[Math.max(0, data.length - 100)]);
+  const xExtents = [min, max + 5];
+
+  const gridHeight = height - margin.top - margin.bottom;
+
+  const elderRayHeight = 100;
+  const elderRayOrigin = (_, h) => [0, h - elderRayHeight];
+  const barChartHeight = gridHeight / 4;
+  const barChartOrigin = (_, h) => [0, h - barChartHeight - elderRayHeight];
+  const chartHeight = gridHeight - elderRayHeight;
+  const yExtents = (data) => {
+    return [data.high, data.low];
+  };
+  const dateTimeFormat = "%d %b";
+  const timeDisplayFormat = timeFormat(dateTimeFormat);
+
+  const barChartExtents = (data) => {
+    return data.volume;
+  };
+
+  const candleChartExtents = (data) => {
+    return [data.high, data.low];
+  };
+
+  const yEdgeIndicator = (data) => {
+    return data.close;
+  };
+
+  const volumeColor = (data) => {
+    return data.close > data.open
+      ? "rgba(38, 166, 154, 0.3)"
+      : "rgba(239, 83, 80, 0.3)";
+  };
+
+  const volumeSeries = (data) => {
+    return data.volume;
+  };
+
+  const openCloseColor = (data) => {
+    return data.close > data.open ? "#26a69a" : "#ef5350";
+  };
+
+  // 차트 너비
+  const chartWidth = chartContainerRef.current
+    ? chartContainerRef.current.clientWidth
+    : null;
 
   return (
-    <div className="chart-container">
-      <Button
-        onClick={() => setIsCandleChart(!isCandleChart)}
-        size="small"
-        sx={{
-          backgroundColor: "white",
-          color: "black",
-          borderColor: "black",
-          minWidth: "10%",
-          "&:hover": { backgroundColor: "#F0F0F0", borderColor: "black" },
-        }}
-      >
-        [자세한 차트 보기]
-      </Button>
-      <ApexChart
-        type={isCandleChart ? "candlestick" : "line"}
-        series={[
-          {
-            name: "Stock Price",
-            data: seriesData,
-          },
-        ]}
-        options={{
-          colors: [chartColor],
-          chart: {
-            height: 500,
-            width: "100%",
-            toolbar: {
-              tools: {},
-            },
-            background: "transparent",
-          },
-          fill: {
-            type: "solid",
-          },
-          stroke: {
-            curve: isCandleChart ? undefined : "smooth",
-            width: 2.8,
-          },
-          grid: {
-            show: false,
-          },
-          xaxis: {
-            type: "datetime",
-            axisBorder: {
-              show: false,
-            },
-            axisTicks: {
-              show: false,
-            },
-          },
-          yaxis: {
-            show: false,
-          },
-        }}
-      />
-
-      <div className="btn-container" style={{ textAlign: "center" }}>
-        <Link to={`/detail/${stockCode}/chart/daily`} style={{ textDecoration: "none" }}>
-          <Button
-            variant="outlined"
-            sx={{
-              backgroundColor: "white",
-              color: "black",
-              borderColor: "black",
-              minWidth: "80%",
-              "&:hover": { backgroundColor: "#F0F0F0", borderColor: "black" },
-            }}
+    <div ref={chartContainerRef} style={{ width: "100%", height: "100%" }}>
+      {chartWidth && (
+        <ChartCanvas
+          height={height}
+          ratio={3}
+          width={chartWidth}
+          margin={margin}
+          data={data}
+          displayXAccessor={displayXAccessor}
+          seriesName="Data"
+          xScale={xScale}
+          xAccessor={xAccessor}
+          xExtents={xExtents}
+          zoomAnchor={lastVisibleItemBasedZoomAnchor}
+        >
+          <Chart
+            id={2}
+            height={barChartHeight}
+            origin={barChartOrigin}
+            yExtents={barChartExtents}
           >
-            일별 & 실시간 시세
-          </Button>
-        </Link>
-      </div>
+            <BarSeries fillStyle={volumeColor} yAccessor={volumeSeries} />
+          </Chart>
+
+          <Chart id={3} height={chartHeight} yExtents={candleChartExtents}>
+            <XAxis showGridLines showTickLabel={false} />
+            <YAxis showGridLines tickFormat={pricesDisplayFormat} />
+            <CandlestickSeries />
+            <LineSeries
+              yAccessor={ema26.accessor()}
+              strokeStyle={ema26.stroke()}
+            />
+            <CurrentCoordinate
+              yAccessor={ema26.accessor()}
+              fillStyle={ema26.stroke()}
+            />
+            <LineSeries
+              yAccessor={ema12.accessor()}
+              strokeStyle={ema12.stroke()}
+            />
+            <CurrentCoordinate
+              yAccessor={ema12.accessor()}
+              fillStyle={ema12.stroke()}
+            />
+            <MouseCoordinateY
+              rectWidth={margin.right}
+              displayFormat={pricesDisplayFormat}
+            />
+            <EdgeIndicator
+              itemType="last"
+              rectWidth={margin.right}
+              fill={openCloseColor}
+              lineStroke={openCloseColor}
+              displayFormat={pricesDisplayFormat}
+              yAccessor={yEdgeIndicator}
+            />
+            <MovingAverageTooltip
+              origin={[8, 24]}
+              options={[
+                {
+                  yAccessor: ema26.accessor(),
+                  type: "EMA",
+                  stroke: ema26.stroke(),
+                  windowSize: ema26.options().windowSize,
+                },
+                {
+                  yAccessor: ema12.accessor(),
+                  type: "EMA",
+                  stroke: ema12.stroke(),
+                  windowSize: ema12.options().windowSize,
+                },
+              ]}
+            />
+
+            <ZoomButtons />
+            <OHLCTooltip origin={[8, 16]} />
+          </Chart>
+          <Chart
+            id={4}
+            height={elderRayHeight}
+            yExtents={[0, elder.accessor()]}
+            origin={elderRayOrigin}
+            padding={{ top: 8, bottom: 8 }}
+          >
+            <ElderRaySeries yAccessor={elder.accessor()} />
+
+            <XAxis showGridLines gridLinesStrokeStyle="#e0e3eb" />
+            <YAxis ticks={4} tickFormat={pricesDisplayFormat} />
+
+            <MouseCoordinateX displayFormat={timeDisplayFormat} />
+            <MouseCoordinateY
+              rectWidth={margin.right}
+              displayFormat={pricesDisplayFormat}
+            />
+
+            <SingleValueTooltip
+              yAccessor={elder.accessor()}
+              yLabel="Elder Ray"
+              yDisplayFormat={(d) =>
+                `${pricesDisplayFormat(d.bullPower)}, ${pricesDisplayFormat(
+                  d.bearPower
+                )}`
+              }
+              origin={[8, 16]}
+            />
+          </Chart>
+          <CrossHairCursor />
+        </ChartCanvas>
+      )}
     </div>
   );
 }
