86f5617163274e8d8f7d5c19020d3f7a5c7a200a
YeongseoYoo <youngseo1314@sookmyung.ac.kr>
2024-03-25 16:28:42 +0900

    feat: Top Keyword 페이지 구현

--- src/routes/Main/MainPage.jsx
+++ src/routes/Main/MainPage.jsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import HotTopic from "./HotTopic";
 import styles from "./MainPage.module.css";
 import PopularStock from "./PopularStock";
+import TopKeyword from "./TopKeyword";
 export default function MainPage() {
   return (
     <div className={styles.layout}>
       <div className={styles.hotTopic}>
         <HotTopic />
       </div>
+      <TopKeyword />
       <PopularStock />
     </div>
   );
---
+++ src/routes/Main/TopKeyword.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getKeywords } from "../../lib/apis/Shinhan";
+import Keyword from "./keyword.png";
+import styles from "./PopularStock.module.css";
+
+export default function TopKeyword() {
+  const [keywords, setKeywords] = useState([]);
+
+  useEffect(() => {
+    const fetchKeywords = async () => {
+      try {
+        const response = await getKeywords();
+        // response는 객체이므로 그 안에 있는 키워드 정보를 뽑아냅니다.
+        const keywordList = response.map(item => item.keyword);
+        setKeywords(keywordList);
+      } catch (error) {
+        console.error("Error fetching keywords:", error);
+      }
+    };
+
+    fetchKeywords();
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <img src={Keyword} className={styles.chart} alt="" />
+        <div className={styles.text}>인기 키워드</div>
+      </div>
+      <br />
+      <div className={styles.keywordRow}>
+        {keywords.map((keyword, index) => (
+          <div key={index} className={styles.keywordItem}>
+            <Link to={`/keyword/${keyword}`}>{keyword}</Link>
+          </div>
+        ))}
+      </div>
+      <br />
+    </div>
+  );
+}
